335539d30f374fbc8a57afa8cdaf9fd35a437829
Dimitris Poulopoulos <dpoulopoulos@outlook.com>
2020-02-04 10:06:01 +0200

    doc: Adds metric documentation.

--- cf_step/metrics.py
+++ cf_step/metrics.py
@@ -3,14 +3,19 @@
 __all__ = ['recall_at_k', 'precision_at_k']
 
 # Cell
-def recall_at_k(predictions, targets, k):
+from typing import List
+
+# Cell
+def recall_at_k(predictions: List[int], targets: List[int], k: int):
+    """Computes `Recall@k` from the given predictions and targets sets."""
     predictions_set = set(predictions[:k])
     targets_set = set(targets)
     result = len(targets_set & predictions_set) / float(len(targets_set))
     return result
 
 # Cell
-def precision_at_k(predictions, targets, k):
+def precision_at_k(predictions: List[int], targets: List[int], k: int):
+    """Computes `Precision@k` from the given predictions and targets sets."""
     predictions_set = set(predictions[:k])
     targets_set = set(targets)
     result = len(targets_set & predictions_set) / float(len(predictions_set))
